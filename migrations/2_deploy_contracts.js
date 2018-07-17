96fdf5fe5ad8a978d96c4e9727ad8cd167a7c179
Surya Bakshi <sbaks0820@gmail.com>
2018-07-17 18:27:56 -0400

    migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const BattleShip = artifacts.require('./BattleShips.sol')
+const SimpleSprites = artifacts.require('./SimplifiedSprites.sol')
+const StateChannel = artifacts.require('./StateChannel.sol')
 
 module.exports = async (deployer, network, accounts) => {
-    deployer.deploy(BattleShip, accounts[1], accounts[2])
+    await deployer.deploy(BattleShip, accounts[1], accounts[2])
 }
