d9fe1453c3077cfd8c26b42bbd95efbd9422d65c
sm2686 <sm2686@cornell.edu>
2018-07-18 12:03:25 -0400

    Add hit/ miss functions

--- plot/plot.py
+++ plot/plot.py
@@ -9,8 +9,10 @@ data_p1 = np.zeros((10, 10))
 data_p2 = np.zeros((10, 10))
 
 # create discrete colormap
-cmap = colors.ListedColormap(['white', 'blue'])
-bounds = [0,10,20]
+cmap = colors.ListedColormap(['white', 'green', 'red'])
+bounds = [0,5,15,20]
+hit_num = 10
+miss_num = 20
 norm = colors.BoundaryNorm(bounds, cmap.N)
 
 fig, ax = plt.subplots()
@@ -29,14 +31,27 @@ ax1.grid(which='major', axis='both', linestyle='-', color='k', linewidth=2)
 ax1.set_xticks(np.arange(-.5, 10, 1));
 ax1.set_yticks(np.arange(-.5, 10, 1));
 
-def set_data(i, j, val, b, data):
-    data[i][j] = val
-    b.set_data(data)
+def hit(i, j, player):
+    if player == 1:
+        data_p1[i][j] = hit_num
+        grid_p1.set_data(data_p1)
+    else:
+        data_p2[i][j] = hit_num
+        grid_p2.set_data(data_p2)
+    plt.pause(0.5)
+
+def miss(i, j, player):
+    if player == 1:
+        data_p1[i][j] = miss_num
+        grid_p1.set_data(data_p1)
+    else:
+        data_p2[i][j] = miss_num
+        grid_p2.set_data(data_p2)
     plt.pause(0.5)
 
 for i in range(10):
-    set_data(4, 4, np.random.rand() * 20, grid_p1, data_p1)
-    set_data(1, 2, np.random.rand() * 20, grid_p2, data_p2)
+    hit(i, 0, 1)
+    miss(0, i, 2)
 
 # set_data(2, 3, np.random.rand() * 20, grid1)
 plt.show()
