cf51303c7b0793b48a1df5e9d9ab7a69987e86ef
Patrick McCorry <stonecoldpat@gmail.com>
2018-08-13 10:11:40 +0800

    Update and rename StateChannel.sol to StateChannelFactory.sol

--- contracts/StateChannelFactory.sol
+++ contracts/StateChannelFactory.sol
@@ -122,3 +122,12 @@ contract StateChannel {
         return bestRound;
     }
 }
+
+// Responsible for creating state channels and forwarding the address back to caller contract. 
+// Can be re-used by any contract. 
+contract StateChannelFactory {
+    
+    function createStateChannel(address[] _parties, uint _disputePeriod) public returns (address addr) {
+        return new StateChannel(_parties, _disputePeriod);
+    }
+}
